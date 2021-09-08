20f6f2148e055fe551d822b275a42dc990616851
ZazBah <zazbah88@gmail.com>
2021-09-09 03:37:15 +0600

    lgran first commit

---
+++ asset/js/navbar.js
@@ -0,0 +1,12 @@
+$('.menu-toggle-btn').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.nav-menu').toggleClass('active');
+
+    $('.nav-menu .link1').toggleClass('animate__animated animate__bounceInDown').css({'animation-delay' : '0.7s'});
+    $('.nav-menu .link2').toggleClass('animate__animated animate__bounceInDown').css({'animation-delay' : '0.9s'});
+    $('.nav-menu .link3').toggleClass('animate__animated animate__bounceInDown').css({'animation-delay' : '1.1s'});
+    $('.nav-menu .link4').toggleClass('animate__animated animate__bounceInDown').css({'animation-delay' : '1.3s'});
+    $('.nav-menu .link5').toggleClass('animate__animated animate__bounceInDown').css({'animation-delay' : '1.5s'});
+    $('.nav-menu .link6').toggleClass('animate__animated animate__bounceInDown').css({'animation-delay' : '1.7s'});
+
+})
